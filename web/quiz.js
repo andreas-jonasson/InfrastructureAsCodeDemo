e42cd80db1e14c2770126e7cad681e97464044a8
Andreas <andreas.jonasson@gmail.com>
2023-12-09 18:29:20 +0100

    Worked with the quiz UI. Buggy answer checks.

--- web/quiz.js
+++ web/quiz.js
@@ -2,10 +2,13 @@ const app = Vue.createApp({
 
     data() {
         return {
+            quizMode: true,
             cards: [],
             currentCard: 0,
             question: "Question",
-            options: [ "Option one", "Option two", "Option three", "Option four" ]
+            options: [ "Option one", "Option two", "Option three", "Option four" ],
+            selected: [],
+            answers: []
          }
     },
 
@@ -13,6 +16,12 @@ const app = Vue.createApp({
         cards(val) {
             this.question = val[this.currentCard].question
             this.options = val[this.currentCard].options
+            this.answers = val[this.currentCard].correctOptions
+        },
+        currentCard(val) {
+            this.question = this.cards[this.currentCard].question
+            this.options = this.cards[this.currentCard].options
+            this.answers = this.cards[this.currentCard].correctOptions
         }
     },
 
@@ -20,6 +29,43 @@ const app = Vue.createApp({
     },
 
     methods: {
+        previousQuestion() {
+            console.log('Previous question')
+            this.currentCard--
+            if (this.currentCard < 0)
+                this.currentCard = this.cards.length - 1
+
+            this.selected = []
+            console.log(this.currentCard)
+        },
+        nextQuestion() {
+            console.log('Next question')
+            this.currentCard++
+            if (this.currentCard >= this.cards.length)
+                this.currentCard = 0
+
+            this.selected = []
+            console.log(this.currentCard)
+        },
+        showQuestion() {
+            console.log('Show question')
+            this.quizMode = true
+        },
+        showAnswer() {
+            console.log('Show answer')
+            this.quizMode = false
+        },
+        isCorrect(i) {
+            console.log(this.selected[i])
+            console.log(this.answers[i])
+
+            if (typeof this.selected[i] === 'undefined')
+                return false
+            if (this.selected[i] && this.answers[i] == 1 || !this.selected[i] && this.answers[i] == 0)
+                return true
+            else
+                return false
+        }
     },
 
     mounted() {
